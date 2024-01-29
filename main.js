eaf995d757a425538db4196f120e68f780d64e0c
gauharzaman110 <142487253+gauharzaman110@users.noreply.github.com>
2024-01-29 20:04:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: " What do you want to add in you todo ?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add more todo ?",
+            default: false
+        }
+    ]);
+    const { addMore, TODO } = answer;
+    loop = addMore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your Todo list: ");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos Found");
+}
